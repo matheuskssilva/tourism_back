39d9f818fe1a85140259211895a865a42bb4e7bd
Matheus Silva <matheuskauan2709@gmail.com>
2024-10-14 23:48:40 -0300

    first commit api amadeus

---
+++ src/main.ts
@@ -0,0 +1,56 @@
+import express, { Request, Response } from 'express';
+import Amadeus from 'amadeus';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('API is running');
+});
+
+const amadeus = new Amadeus({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+});
+
+app.post('/api/flightSearch', async (req: Request, res: Response) => {
+    const { origin, destination, checkint, checkout, passengers, duration } = req.body as {
+        origin: string;
+        destination: string;
+        checkint: string;
+        checkout: string;
+        passengers: number;
+        duration: string
+    };
+
+    console.log('Incoming request data:', req.body);
+
+    try {
+        const response = await amadeus.shopping.flightOffersSearch.get({
+            originLocationCode: origin,
+            destinationLocationCode: destination,
+            departureDate: checkint,
+            returnDate: checkout,
+            adults: passengers.toString(),
+            duration: duration
+        });
+        console.log('Amadeus response:', response.result); // Log para verificar os dados
+        res.json(response.result); // Certifique-se de que está retornando os dados corretos
+    } catch (error) {
+        console.error('Error fetching flight offers:', error.response?.data || error);
+        res.status(500).json({ error: 'Failed to fetch flight offers', details: error.response?.data || error });
+    }
+});
+
+const PORT = parseInt(process.env.PORT) || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
